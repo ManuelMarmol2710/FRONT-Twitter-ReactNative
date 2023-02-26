3f5c729e8cde0a8b5ab25abdd4275900690ddc91
ManuelMarmol2710 <92897105+ManuelMarmol2710@users.noreply.github.com>
2023-02-26 16:48:33 -0400

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -7,15 +7,12 @@ import EditProfilePage from "./src/pages/EditProfile";
 import SendEmailPage from "./src/pages/SendEmail";
 import BuscarPage from "./src/pages/BuscarPage";
 import AwayProfile from "./src/pages/AwayProfile";
+import SiguiendoPage from "./src/pages/Siguiendo";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React, { useState } from 'react';
-
 const Stack = createStackNavigator();
-
 export default function App() {
-
-const isAuth = useAuthStore((state) => state.isAuth);
+  const isAuth = useAuthStore((state) => state.isAuth);
 
   return (
     <NavigationContainer>
@@ -23,14 +20,16 @@ const isAuth = useAuthStore((state) => state.isAuth);
       headerShown: false,
       cardStyle: { backgroundColor: '#afc7d8' }
   }}>
+   
         <Stack.Screen name="login" component={LoginPage} />
         <Stack.Screen name="register" component={RegisterPage} />
         <Stack.Screen name="profile" component={ProfilePage} />
-        <Stack.Screen name="homepage" component={HomePage} />
         <Stack.Screen name="sendEmail" component={SendEmailPage} />
         <Stack.Screen name="EditProfile" component={EditProfilePage} />
-        <Stack.Screen name="buscar" component={BuscarPage} />
         <Stack.Screen name="awayprofile" component={AwayProfile} />
+        <Stack.Screen name="buscar" component={BuscarPage}  />
+        <Stack.Screen name="follow" component={SiguiendoPage}  />
+        <Stack.Screen name="homepage" component={HomePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
